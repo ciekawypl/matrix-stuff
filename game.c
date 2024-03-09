89a1f9b49f77d506f41f84529e483058791995e7
ciekawy <vajjperr21@gmail.com>
2024-03-09 01:07:11 +0100

    code refacturing and generalization

--- game.c
+++ game.c
@@ -2,78 +2,158 @@
 #define RAYGUI_IMPLEMENTATION
 #include <raygui.h>
 
-int main(void){
-    const int screenWidth = 800;
-    const int screenHeight = 450;
 
-    InitWindow(screenWidth, screenHeight, "Testowe okno");
-    
+#define TABLE_SIZE 3
 
-    //      test data to render
-    // int table[3][3] = {{1,2,3}, {4,5,6}, {7,8,9}};
+typedef struct Table{
+    Vector2 position;
+    int value;
+} Table;
+
+typedef struct DragBox{
+    Vector2 position;
+    Vector2 defPosition;
+    bool isHeld;
+} DragBox;
+
+typedef struct DragSlot{
+    Vector2 position;
+    Color color;
+    int operant;
+    bool isVisible;
+} DragSlot;
 
 
-    //      default draggable circile position
-    int circx = 200;
-    int circy = 200;
+const int screenWidth = 800;
+const int screenHeight = 450;
 
-    bool isHeld = false;
-    bool dropped = false;
 
+static bool mouseDraggs = false;
+
+
+static Table table[TABLE_SIZE][TABLE_SIZE] = {0};
+static DragBox dragBox[TABLE_SIZE] = {0};
+static DragSlot dragSlot[2][TABLE_SIZE] = {0};
+
+
+static void initGame(void);
+static void drawFrame(void);
+static void updateLogic(void);
+
+
+int main(){
+    InitWindow(screenWidth, screenHeight, "Matrix Calculator");
+
+    initGame();
 
     SetTargetFPS(60);
 
     while (!WindowShouldClose()){
-
-        //      handles dragged circle position
-        if (isHeld && IsMouseButtonDown(0)){
-            circx = GetMouseX();
-            circy = GetMouseY();
-
-            DrawRectangle(300, 300, 50, 50, BLACK);
-        } else{
-
-            //      tells to draw blue circle if dragged circle has been dropped on the correct spot
-            if (CheckCollisionCircleRec((Vector2){circx, circy}, 20, (Rectangle){300, 300, 50, 50})){
-                dropped = true;
-            }
-
-            isHeld = false;
-            circx = 200;
-            circy = 200;
-        }
-
-        //      sets the circle as being held
-        if (IsMouseButtonDown(0) && CheckCollisionPointCircle(GetMousePosition(), (Vector2){circx, circy}, 20)){
-            isHeld = true;
-        }
-
-        //      renders only when dragged circle touches the black box
-        if (CheckCollisionCircleRec((Vector2){circx, circy}, 20, (Rectangle){300, 300, 50, 50})){
-            DrawRectangle(500, 100, 50, 50, GREEN);
-        }
-
-        BeginDrawing();
-            ClearBackground(RAYWHITE);
-            GuiGrid((Rectangle){0, 0, screenWidth, screenHeight}, "dupa", 50, 1, &(Vector2){0,0});
-
-            //      draws only when dragged circle has been dropped on the black box
-            if (dropped){
-                DrawCircle(500, 200, 20, BLUE);
-            }
-            
-            DrawCircle(circx,circy,20,BLACK);
-
-
-            //      playing around with matrix rendering
-            // for (int i = 0; i < sizeof(table)/sizeof(table[0]); i++){
-                // for (int j = 0; j < sizeof(table)/sizeof(table[0]); j++){
-                    // DrawText(TextFormat("%d", table[i][j]), screenWidth/2+20*j, 20+20*i, 20, BLACK);
-                // }
-            // }
-        EndDrawing();
+        updateLogic();
+        drawFrame();
     }
-
+    
     CloseWindow();
     return 0;
+}
+
+void initGame(void){
+    mouseDraggs = false;
+    for (int i = 0; i < TABLE_SIZE; i++){
+        for (int j = 0; j < TABLE_SIZE; j++){
+            table[i][j].position = (Vector2){200+30*i, 200+30*j};
+            table[i][j].value = GetRandomValue(1, 9);
+        }
+    }
+
+    for (int i = 0; i < TABLE_SIZE; i++){
+        dragBox[i].position = (Vector2){table[0][i].position.x - 30, table[0][i].position.y};
+        dragBox[i].defPosition = dragBox[i].position;
+        dragBox[i].isHeld = false;
+    }
+    
+    for (int i = 0; i < TABLE_SIZE; i++){
+        dragSlot[i][0].operant = 0;
+        dragSlot[i][0].color = BLUE;
+        dragSlot[i][0].position = (Vector2){dragBox[i].position.x - 60, dragBox[i].position.y};
+    
+        dragSlot[i][1].operant = 1;
+        dragSlot[i][1].color = RED;
+        dragSlot[i][1].position = (Vector2){dragBox[i].position.x - 30, dragBox[i].position.y};
+    }
+}
+
+void updateLogic(void){
+    if (!IsMouseButtonDown(0)){
+        mouseDraggs = false;
+    }
+
+    //      handles dragging status
+    if (!mouseDraggs){
+        for (int i = 0; i < TABLE_SIZE; i++){
+            if (IsMouseButtonDown(0) && CheckCollisionPointRec(GetMousePosition(), (Rectangle){dragBox[i].position.x, dragBox[i].position.y, 20, 20})){
+                dragBox[i].isHeld = true;
+                mouseDraggs = true;
+            }
+        }
+    }
+
+    //      handles dragged box position
+    for (int i = 0; i < TABLE_SIZE; i++){
+        if (IsMouseButtonDown(0) && dragBox[i].isHeld){
+            dragBox[i].position = GetMousePosition();
+        } else{
+            dragBox[i].isHeld = false;
+            // isDragged = false;
+            dragBox[i].position = dragBox[i].defPosition;
+        }
+    }
+
+    //      handles visibiltiy of slots
+    for (int i = 0; i < TABLE_SIZE; i++){
+        if (dragBox[i].isHeld){
+            for (int j = 0; j < TABLE_SIZE; j++){
+                dragSlot[j][0].isVisible = true;
+            }
+            dragSlot[i][0].isVisible = false;
+            
+        } else if (!IsMouseButtonDown(0)){
+            dragSlot[i][0].isVisible = false;
+        }
+    }
+}
+
+void drawFrame(void){
+    BeginDrawing();
+        ClearBackground(WHITE);
+
+        //      renders the table
+        for (int i = 0; i < TABLE_SIZE; i++){
+            for (int j = 0; j < TABLE_SIZE; j++){
+                DrawText(TextFormat("%d", table[i][j].value), table[i][j].position.x, table[i][j].position.y, 20, BLACK);
+            }
+        }
+
+
+        //      renders hideable slots
+        for (int i = 0; i < TABLE_SIZE; i++){
+            if (dragSlot[i][0].isVisible){
+                DrawRectangle(dragSlot[i][0].position.x, dragSlot[i][0].position.y, 20, 20, dragSlot[i][0].color);
+                DrawRectangle(dragSlot[i][1].position.x, dragSlot[i][1].position.y, 20, 20, dragSlot[i][1].color);
+                
+                DrawText(TextFormat("+"), dragSlot[i][1].position.x+5, dragSlot[i][1].position.y+1, 20, WHITE);
+                DrawText(TextFormat("-"), dragSlot[i][0].position.x+6, dragSlot[i][0].position.y+1, 20, WHITE);
+            }
+        }
+        
+        //      renders draggable boxes
+        for (int i = 0; i < TABLE_SIZE; i++){
+            if (!dragBox[i].isHeld){
+                DrawRectangle(dragBox[i].position.x, dragBox[i].position.y, 20, 20, BLACK);
+            } else{
+                DrawRectangle(dragBox[i].position.x, dragBox[i].position.y, 20, 20, GREEN);
+            }
+            DrawText(TextFormat("="), dragBox[i].position.x+6, dragBox[i].position.y+1, 20, WHITE);
+        }
+    EndDrawing();
 }
